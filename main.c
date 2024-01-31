1c3e4c4b39b8b8ec735d32fbec08141dd7a81c0e
Lion <development@kortlepel.com>
2024-01-31 12:26:16 +0100

    add comments

--- main.c
+++ main.c
@@ -3,19 +3,26 @@
 #include <stdio.h>
 #include <string.h>
 
+// state of the state machine used to decide what to write
+// to stdout and what to skip
 enum State {
     STATE_NORMAL,
     STATE_SKIPPING,
 };
 
+// buffer used to read file contents into
 static char in_buf[4096];
+// buffer used to write results into
 static char out_buf[4096];
 
+// compares if str1 starts with str2
 bool safe_str_startswith(const char* str1, size_t str1len, const char* str2) {
     size_t str2len = strlen(str2);
     return strncmp(str1, str2, (str2len < str1len ? str2len : str1len)) == 0;
 }
 
+// returns true for any strings which match a control sequence
+// output by logkeys. this list may not be complete.
 bool is_control(const char* str, size_t len) {
     return safe_str_startswith(str, len, "Enter")
         || safe_str_startswith(str, len, "Up")
@@ -41,20 +48,29 @@ bool is_control(const char* str, size_t len) {
         || (safe_str_startswith(str, len, "#+"));
 }
 
+// does the cleanup on a file, writing output to stdout.
+// removes all control characters.
+// returns 0 on success, non-zero on failure.
 int run(FILE* in) {
     size_t n = 0;
-    // what to skip to (inclusive) when in STATE_SKIPPING
     // little tiny state machine :^)
     enum State state = STATE_NORMAL;
+    // keep reading until feof. ferror doesn't break the loop
+    // and instead returns 1.
     for (;;) {
+        // read as much as fits into the in_buf. the return value indicates
+        // how much was read, and whether an error occurred.
         n = fread(in_buf, 1, sizeof(in_buf), in);
+        // keeps track of how many characters were written into the output
         size_t out_n = 0;
-        // error
+        // if an error occurred, return immediately
         if (n != sizeof(in_buf) && ferror(in) != 0) {
             perror("fread");
             return 1;
         } else {
-            // n bytes were read, iterate through all
+            // n bytes were read, iterate through all of them.
+            // copy all bytes from the input to the output that are
+            // not part of a control sequence such as `<LShft>`.
             for (size_t i = 0; i < n; ++i) {
                 switch (state) {
                 case STATE_NORMAL:
@@ -67,17 +83,22 @@ int run(FILE* in) {
                     }
                     break;
                 case STATE_SKIPPING:
+                    // skips until this condition is met
                     if (in_buf[i] == '>') {
                         state = STATE_NORMAL;
                     }
                     break;
                 }
             }
+            // write only exactly out_b bytes of out_buf to stdout.
+            // if this fails, exit early with failure.
             size_t wrote_n = fwrite(out_buf, 1, out_n, stdout);
             if (wrote_n != out_n && ferror(stdout) != 0) {
                 perror("fwrite");
                 return 1;
             }
+            // end of input file is the only condition on which we can exit
+            // with success.
             if (feof(in) != 0) {
                 break;
             }
@@ -88,8 +109,10 @@ int run(FILE* in) {
 
 int main(int argc, char** argv) {
     if (argc == 1) {
+        // read from stdin (pipe behavior)
         return run(stdin);
     } else if (argc == 2) {
+        // read from file provided via args
         FILE* file = fopen(argv[1], "r");
         if (!file) {
             perror("fopen");
